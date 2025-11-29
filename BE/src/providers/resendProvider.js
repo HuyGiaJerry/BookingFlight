8c064ebfb6940ceefd8e6b7c0ab8b3b78b84b56f
Hoang Trung Hieu <0983814204a@gmail.com>
2025-11-29 08:53:00 +0700

    delete resend key

--- BE/src/providers/resendProvider.js
+++ BE/src/providers/resendProvider.js
@@ -1,17 +1,15 @@
 const { Resend } = require('resend');
 
-const apiKey = 're_Hhs4PzKT_DzMEuKEbVoAB1CaqiLfXD9Rp';
 
-const ADMIN_EMAIL = 'Booking-Flight@booking-flight-app.io.vn'
-
-const resendInstance = new Resend(apiKey)
+console.log('API key', process.env.RESEND_API_KEY)
+const resendInstance = new Resend(process.env.RESEND_API_KEY)
 
 // Function to resend email
 
 async function sendEmail(to, subject, html) {
     try {
         const data = await resendInstance.emails.send({
-            from: ADMIN_EMAIL,
+            from: process.env.ADMIN_EMAIL,
             to,  // nếu chưa verify domain thì chỉ gửi được email này
             subject,
             html
